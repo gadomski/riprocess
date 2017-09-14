c408359f1dee561726e2740ad5c3ab81b871c4ed
Pete Gadomski <pete.gadomski@gmail.com>
2017-09-14 11:44:11 -0600

    Add the executable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+extern crate docopt;
+extern crate riprocess;
+#[macro_use]
+extern crate serde_derive;
+
+use std::path::PathBuf;
+
+const USAGE: &'static str = "
+Query and/or generate material for RiPROCESS projects.
+
+Usage:
+    riprocess image-list <config>
+
+Options:
+    -h --help           Show this screen.
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    cmd_image_list: bool,
+    arg_config: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    use docopt::Docopt;
+    use riprocess::Config;
+
+    let args: Args = Docopt::new(USAGE).and_then(|d| d.deserialize()).unwrap_or_else(|e| e.exit());
+
+    if args.cmd_image_list {
+        let config = Config::from_path(args.arg_config).unwrap();
+        for image in config.image_list().unwrap() {
+            println!("{:.6};{}", image.timestamp, image.path.display());
+        }
+    }
 }
